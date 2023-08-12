5e1fe5d4a00abe16365c7893f967633677afc9ea
MonaLisa <bruiz22>
2023-08-12 12:41:57 -0700

    showing how to use a second file to show date and time

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./myfile');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("Here is an example of inserting date and time..." + dt.myDateTime());
   res.end('Well Hello World!');
 }).listen(8080);
