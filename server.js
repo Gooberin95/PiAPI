ab2865486d87f225703823823454037ac02e3ee0
MonaLisa <bruiz22>
2023-08-12 12:57:25 -0700

    messing with urls

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
-var dt = require('./myfile');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("Here is an example of inserting date and time..." + dt.myDateTime());
-  res.end('Well Hello World!');
+//create a server object:
+http.createServer(function  (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(req.url); //writes a response to client
+	res.end(); //ends response
 }).listen(8080);
+
