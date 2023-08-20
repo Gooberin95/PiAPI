f9cfd7f02ac5efff86f249546267867cfafe3703
MonaLisa <bruiz22>
2023-08-20 12:22:08 -0700

    sending html

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const expres = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
+app.get("/",( req, res) => {
+	res.status(200).send("<b>Brandon </b><br>Ruiz</b>")
+
+});
+app.listen(3000);
 
